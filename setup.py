20cc416d522ce5a06bdbde17db5aa84f50649df8
e11sy <130844513+e11sy@users.noreply.github.com>
2025-06-30 19:25:26 +0300

    feat(): add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="arg_bot",
+    version="0.1.0",
+    description="Telegram bot for Hardweb community",
+    author="e11sy",
+    author_email="egoramurin@gmail.com",
+    python_requires=">=3.8",
+    install_requires=[
+        "python-telegram-bot>=22.1",
+        "pillow>=11.2.1",
+        "requests>=2.32.4",
+        "aiohttp>=3.12.13",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    entry_points={
+        "console_scripts": [
+            "arg-bot=arg_bot.bot:main",
+        ],
+    },
+)
